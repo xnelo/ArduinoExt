323e67436fb4dcda1aa58054c97761fc51d93378
Spencer Hoffa <xnelo.productions@gmail.com>
2021-05-29 00:31:15 -0600

    Initial commit of Arduino Extension library.

---
+++ ArduinoExt.h
@@ -0,0 +1,15 @@
+////////////////////////////////////////
+// Written By: Spencer Hoffa
+// File: ArduinoExt.h
+// Copyright: (c)2021
+/// \copyright \link LICENSE.md MIT License\endlink 2021 Spencer Hoffa 
+////////////////////////////////////////
+
+#ifndef ARDUINOEXT_ARDUINOEXT_H__2_24_2021
+#define ARDUINOEXT_ARDUINOEXT_H__2_24_2021
+
+#include "ArduinoExtCommon.h"
+#include "Data/Buffer.h"
+#include "Math/Crc16.h"
+
+#endif //ARDUINOEXT_ARDUINOEXT_H__2_24_2021
